781a0e8552acf02ef6c0186aa57a8e3982965fc5
nataliehk <hurstknight@gmail.com>
2024-04-15 00:49:09 +0100

    basic nav set-up & blog index page

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,40 @@
+import React from "react"
+import Link from "next/link"
+import { BlogQueryResult } from "./types"
+import { createClient } from "contentful"
+
+const client = createClient({
+  space: process.env.SPACE_ID,
+  accessToken: process.env.ACCESS_TOKEN,
+})
+
+const getBlogEntries = async ():Promise<BlogQueryResult> => {
+  const entries = await client.getEntries({ content_type: "blog" })
+  return entries
+}
+
+export default async function Blog() {
+  const blogEntries = await getBlogEntries()
+  return (
+    <main className="flex min-h-screen flex-col p-24 gap-y-8">
+      {blogEntries.items.map((singlePost) => {
+        const { slug, title, date } = singlePost.fields
+        return (
+          <div key={slug}>
+            <Link className="group" href={`/articles/${slug}`}>
+              <h2 className="font-extrabold text-xl group-hover:text-blue-500 transition-colors">{title}</h2>
+              <span>
+                Posted on{" "}
+                {new Date(date).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                })}
+              </span>
+            </Link>
+          </div>
+        )
+      })}
+    </main>
+  )
+}
